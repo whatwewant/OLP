238b6f4fcb0e552e28baa457f751de3b3bf66a8b
whatwewant <tobewhatwewant@gmail.com>
2014-05-19 21:40:38 +0800

    account/views and templates/account/login.html

--- account/views.py
+++ account/views.py
@@ -1,7 +1,47 @@
 '''view'''
 
-from django.shortcuts import redirect, render
+#coding=utf-8
 
-def index(request):
-    
-    return render(request, 'index.html', {})
+
+# check if the user exist
+from django.contrib.auth.models import User
+
+# use it to a httpresponse
+import json
+
+
+# json httpResponse
+#def message(status, content):
+#    response_json = {"status": status, "content": content}
+#    return HttpResponse(json.dumps(response_json))
+
+
+def user_exist(username):
+    try:
+        User.objects.get(username=username)
+    except User.DoseNotExist:
+        return False
+    return True
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username","-1")
+        password = request.POST.get("password","-1")
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None and user.is_active:
+            auth.logout(request)
+            auth.login(request, user)
+            next = request.POST.get("next","/")
+            # json httpResponse
+            response_json = {"status" :"success", "redirect" :next}
+            return HttpResponse(json.dumps(response_json))
+        else:
+            response_json = {"status": "error", "content" :u"用户名或密码错误"}
+            return HttpResponse(json.dumps(response_json))
+    else:
+        next = request.GET.get("next","/")
+        return render(request,"account/login.html",{"next" :next })
+
+
+
