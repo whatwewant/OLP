2e58dbec0f133ea132b969d51cb6e2280ce64b4f
liutaotao <lttzzlll@gmail.com>
2014-05-21 17:35:50 +0800

    account/login already tested

--- account/views.py
+++ account/views.py
@@ -1,59 +1,29 @@
-
+#coding=utf-8
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 from django.contrib import auth
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-
+from django.shortcuts import render_to_response, render
 
 def index(request):
-   # return render(request, 'account/login.html')
-    return HttpResponse("this is a login html")
+    return HttpResponse("Welcome to index page")
 
-'''
-def login(request):
-    if request.method == "POST": username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = auth.authenticate(username=username, password=password)
-        
-
-        if User.filter(pk=user.pk).exists():
-            auth.login(request, user)
-            next = request.POST.get("next", '/')
-            #return HttpResponse(200)
-            return render(request, 'accout/index.html')
-            #return HttpResponseRedirect("/account/login")
-
-        #return HttpResponse(500)
-        login_failed_error=" login failed "
-        return render(request, "account/login_failed.html", {"login_failed_error" : login_failed_error })
-
-    next = request.GET.get("next", "/")
-    return render(request, "account/login.html", {"next" : next })
-'''
-
-def login(request):
-    if request.user.is_authenticated():
-        return HttpReponse(u"you are already loggined")
-
+def user_login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(username = username, password = password)
-
         if user is not None :
             if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/account/%d' % user.id)
-        
-            return HttpResponse(u'用户没有启用')
-        
-        return HttpResponse(u'用户名或密码错误')
-    
-    return render_to_response('account/login.html')
+                auth.login(request, user)
+                return HttpResponse('%d' % user.id) 
 
-def logout(request):
-    auth.logout(request)
-    return render_to_response('index.html')
+            error = u'用户没有启用'
+            return render(request, 'account/login.html', {'error' : error})
+        
+        error = u'用户名或密码错误'
+        return render(request, 'account/login.html', {'error' : error})
+    
+    return render(request, 'account/login.html', {})
+
 
