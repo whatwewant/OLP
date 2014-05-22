7ac97b16f3c96f60ee71b153e159ad8d9b03369d
ylp <splsylp@gmail.com>
2014-05-22 18:54:16 +0800

    add register

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
+#encoding:utf-8
+import re
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
-from Forum.models import Post
+from.models import UserProfile
 
 def index(request):    
     return render(request, 'index.html', {})
@@ -16,34 +18,34 @@ def register(request):
         nicename = request.POST.get("nicename")
 
         if not (len(username) >= 3 and len(username) <= 10):
-            return render(request, "register.html", {"error": u"用户名只能是3-10个字符"})
-        
-        name = re.compile("^[_A-Za-z0-9]+$")
+            return render(request, "account/register.html",
+                    {"error": u"用户名只能是3-10个字符"})
+        name = re.compile("^[_A-Za-z0-9\u4e00-\u9fa5]+$")
         if not name.match(username):
-            return render(request, "register.html", {"error": u"用户名只能是数字和英文字符"})
-
-        username_is_exist = User.objects.filter(username = username).exists()
-        if username_is_exist:
-            return render(request, "register.html", {"error": u"用户名已经存在"})
-         
+            return render(request, "account/register.html",
+                    {"error": u'用户名只能是数字、英文字符、下划线和汉字'})
+        if User.objects.filter(username = username).exists():
+            return render(request, "account/register.html",
+                    {"error": u"用户名已经存在"})
         if not(len(nicename) >= 2 and len(nicename) <= 10):
-            return render(request, "register.html", {"error": u"昵称只能是2-10个字符"})
-
+            return render(request, "account/register.html",
+                    {"error": u"昵称只能是2-10个字符"})
         mail = re.compile("[^\._-][\w\.-]+@(?:[A-Za-z0-9]+\.)+[A-Za-z]+$")
         if not mail.match(email):
-            return render(request, "register.html", {"error": u"无效的邮箱格式"})
-
+            return render(request, "account/register.html",
+                    {"error": u"无效的邮箱格式"})
         if password != re_password:
-            return render(request, "register.html", {"error": u"两次输入的密码不一致"})
-
-        User.objects.create_user(username = username, password = password, email = email)
-        UserProfile.objects.create_user(nicename = nicename)
-
+            return render(request, "account/register.html",
+                    {"error": u"两次输入的密码不一致"})
+        user = User.objects.create_user(username=username,
+                password=password, email=email)
+        UserProfile.objects.create(user=user,
+                nicename = nicename)
         #return HttpResponseRedirect("/login/")
-        return render(request, "register_success.html")
-    
-    before = requst.GET.get("before", "/")
-    return render(request, "Account/register.html", {"before": before})
+        return render(request, "account/register_success.html")
+        
+    before = request.GET.get("before", "/")
+    return render(request, "account/register.html", {"before": before})
 
 
 
