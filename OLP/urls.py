dcdf34067c1a6b09190574c5d2f5f9a0198d2524
ylp <splsylp@gmail.com>
2014-05-22 20:51:04 +0800

    实现 sign_in.html sign_up.html

--- OLP/urls.py
+++ OLP/urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
      url(r'^index/$', 'account.views.index', name='index'),
      url(r'^sign_in/$','account.views.sign_in', name='sign_in'),
      url(r'^sign_up/$', 'account.views.sign_up', name='sign_up'),
-	 url(r'^sign_out/$', 'account.views.sign_out', name='sign_out'),
+     url(r'^sign_out/$', 'account.views.sign_out', name='sign_out'),
 )
