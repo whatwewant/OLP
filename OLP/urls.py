2834e586ced30f387514e62bc56a6da80431eca2
potter <huangjinhua4535@gmail.com>
2014-06-04 21:53:21 +0800

    ...

--- OLP/urls.py
+++ OLP/urls.py
@@ -16,16 +16,18 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$', 'account.views.index', name='index'),
 
-	# user 
+    # user 
     url(r'^sign_in/$','account.views.sign_in', name='sign_in'),
     url(r'^sign_up/$', 'account.views.sign_up', name='sign_up'),
     url(r'^sign_out/$', 'account.views.sign_out', name='sign_out'),
 
-	# blog
-	url(r'^$', 'blog.views.homePage', name='homepage'),
+    # blog
+    url(r'^$', 'blog.views.homePage', name='homepage'),
     url(r'^blog/$', 'blog.views.homePage'),
     url(r'^blog/index/$', 'blog.views.homePage'),
     url(r'^blog/write/$', 'blog.views.write', name='blog_write'),
     url(r'^blog/edit/(?P<id>\d+)/$', 'blog.views.edit', name='blog_edit'),
     url(r'blog/(?P<author>\w*)/$', 'blog.views.personPage', name='blog_index'),
+    # article
+    url(r'^post/(?P<author>\w+)/(?P<pk>\d+)/$', 'blog.views.post', name='post'),
 )
